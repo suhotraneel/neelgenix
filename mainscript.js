94d0777c233d17a53da190bba8a294b3da62533e
Neel Genix <suhotrachakraborty@gmail.com>
2024-07-15 17:50:57 +0530

    43

--- mainscript.js
+++ mainscript.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'SuhotraNeel',
-  host: '34.172.70.137',
-  database: 'pgsubscribe',
-  password: 'Project@2003TRAPS',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 document.getElementById('emailForm').addEventListener('submit', (e) => {
