f12c621a4ae9e496c6e80d43508920f04fcb0698
Neel Genix <suhotrachakraborty@gmail.com>
2024-07-15 19:22:07 +0530

    45

---
+++ next.config.js
@@ -0,0 +1,17 @@
+target: 'erverless',
+};```
+**package.json**
+```json
+{
+  "name": "email-collector",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "next",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "^12.0.7",
+    "pg": "^8.7.1"
+  }
+}
