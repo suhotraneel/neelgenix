2c99875a39008f4cae54158d7c049befbd6cad06
Neel Genix <suhotrachakraborty@gmail.com>
2024-07-17 04:49:04 +0530

    335

--- mainscript.js
+++ mainscript.js
@@ -8,3 +8,17 @@ detectHover.addEventListener('mouseover', () => {
 detectHover.addEventListener('mouseout', () => {
     effectHover.querySelector('path').setAttribute('stroke', '#FFFFFF');
 });
+
+
+const divElement = document.querySelector('.foot1');
+
+    divElement.addEventListener('mousemove', function(event) {
+      // Get div dimensions
+      const divRect = divElement.getBoundingClientRect();
+      
+      // Calculate color based on mouse position
+      const hue = (event.clientX / divRect.width) * 360;
+
+      // Update background color
+      divElement.style.backgroundColor = `hsl(${hue}, 100%, 5%)`;
+    });
