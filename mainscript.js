c570b07c192796c0aae6077a3d4911b1984e1db5
suhotraneel <suhotrachakraborty@gmail.com>
2024-10-07 01:08:13 +0530

    hubdbhw

--- mainscript.js
+++ mainscript.js
@@ -7,19 +7,11 @@ window.addEventListener("load", function() {
 });
 
 // JavaScript to load header
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header').innerHTML = data;
-
-        // Execute the script inside header.html
-        const scripts = document.getElementById('header').getElementsByTagName('script');
-        for (let i = 0; i < scripts.length; i++) {
-            const script = document.createElement('script');
-            script.innerHTML = scripts[i].innerHTML; // Assign the script content
-            document.body.appendChild(script); // Append to body to execute it
-        }
-    })
+fetch('navbar.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+});
 
 
 let lastScrollTop = 0; // Keeps track of the last scroll position
